f67b457eb5355009f631a81a153146831c1a936b
Jon Wire <iambipedal@gmail.com>
2025-10-16 15:34:11 -0500

    add auth

--- packages/backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ packages/backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "missingmanifestce5aa8e5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
